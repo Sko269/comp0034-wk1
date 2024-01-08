99a400093504cea46c88a3c52f2cbf8f47f06869
Shane Pongpanich <zcemspo@ucl.ac.uk>
2024-01-08 15:06:32 +0000

    Created the Paralympics Flask App

---
+++ paralympics/paralympics_flask_app.py
@@ -0,0 +1,15 @@
+# Import the Flask and escape
+from flask import Flask
+from markupsafe import escape
+
+paralympics_app = Flask(__name__)
+
+@paralympics_app.route('/<name>')
+def hello_world(name):
+    return f'Hello {escape(name)} and Welcome to the Paralympics App'
+
+# Run the app
+if __name__ == '__main__':
+    paralympics_app.run(debug=True)
+
+# Run by using the link http://127.0.0.1:5000/name eg http://127.0.0.1:5000/Shane
